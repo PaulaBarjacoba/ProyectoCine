163c2fe6ab20879ac1117c0532762cf4c50fcb2e
Paula Barjacoba <pa.barjacoba@duocuc.cl>
2026-05-24 15:17:23 -0400

    fix de typo en usuariocontroller

--- usuario-service/src/main/java/proyecto/usuario_service/controller/UsuarioController.java
+++ usuario-service/src/main/java/proyecto/usuario_service/controller/UsuarioController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
-public class git stashUsuarioController {
+public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
